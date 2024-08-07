eae93a930617a453b4f61f024a0fdb1c11e70a0c
Akshay946187 <aki748434@gmail.com>
2024-08-07 19:10:58 +0530

    verseljson file

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ app.use("/uploads/files",express.static("uploads/files"))
 app.use(cookieParser())
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.use('/api',authrouter)
 app.use('/api/contacts',contactsRoute)
